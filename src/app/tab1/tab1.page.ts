8cb1602b081cb328d1b0f2be9c3cad8631f9a519
adash333 <adash333@hotmail.com>
2019-09-01 23:10:38 +0900

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker, icon } from 'leaflet';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  map: Map;
 
-  constructor() {}
+  ionViewDidEnter() { 
+    this.leafletMap();
+  }
 
+  leafletMap() {
+    // 地図表示
+    this.map = new Map('mapId').setView([35.685988,139.753947], 15);
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+    marker([35.685988,139.753947], {
+      icon: icon({
+          iconSize: [ 25, 41 ],
+          iconAnchor: [ 13, 41 ],
+          iconUrl: 'assets/images/marker-icon.png',
+          shadowUrl: 'assets/images/marker-shadow.png'
+      })
+    }).addTo(this.map);
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 }
